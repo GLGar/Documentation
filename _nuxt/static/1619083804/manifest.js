4445725a4676581dab9ba7779111fc11970942f6
besoeasy <besoeasy@users.noreply.github.com>
2021-04-22 09:30:08 +0000

    Deploying to dist from  @ 4567a02fedf98abbf35e2a331c22faf18a356ee7 🚀

---
+++ _nuxt/static/1619083804/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FUsage","\u002Fcompare","\u002FModule\u002Ftoken","\u002FModule\u002Fsolver","\u002FContract\u002Fcontroller","\u002FModule\u002Fbridge","\u002FContract\u002Fexpansion","\u002FContract\u002Fsafe","\u002FModule\u002Fwrapped","\u002FContract\u002Fmapping"]})
