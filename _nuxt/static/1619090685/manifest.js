1752a3477a953cce715cdbb8a885f76858e73bab
SuhailCM <77317232+SuhailCM@users.noreply.github.com>
2021-04-22 11:24:49 +0000

    Deploying to dist from  @ d55033c4ee73635dfe23f7c628eb8406d18f7c33 🚀

---
+++ _nuxt/static/1619090685/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fusage","\u002Fcompare","\u002FModule\u002Fbridge","\u002FContract\u002Fexpansion","\u002FContract\u002Fcontroller","\u002FModule\u002Fsolver","\u002FModule\u002Fwrapped","\u002FContract\u002Fmapping","\u002FModule\u002Ftoken","\u002FContract\u002Fsafe"]})
